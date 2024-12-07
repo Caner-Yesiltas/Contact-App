4559588e2b37ee3d46724cf4a3b1ccf280fd225a
CanerYesiltas <caneryesiltas1@gmail.com>
2024-12-07 10:39:21 -0500

    add components and home page

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
+import Home from './pages/Home';
 import './styles/App.css';
 
 function App() {
-  return <div></div>;
+  return <div>
+    <nav className='bg-slate-300 text-center text-2xl cursor-pointer' >
+      Contact App
+    </nav>
+    <Home/>
+  </div>;
 }
 
 export default App;
